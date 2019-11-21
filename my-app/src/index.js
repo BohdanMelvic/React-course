21c0ab07c858d1004e506c849c00a7d1a502f29f
Bohdan Melvic <bohdanmelvic@gmail.com>
2019-11-21 18:39:03 +0200

    restructed project

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 //import AssigmentSolutionModule1 from './AssigmentSolutionModule1';
 //import AssigmentSolutionModule2 from './AssigmentSolutionModule2';
 import * as serviceWorker from './serviceWorker';
