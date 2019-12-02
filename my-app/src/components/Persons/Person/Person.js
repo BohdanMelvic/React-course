aa80552478eb45fb37c6a957f54fe5b73f0d35aa
Bohdan Melvic <bohdanmelvic@gmail.com>
2019-12-02 13:27:03 +0200

    amemded error message

--- my-app/src/components/Persons/Person/Person.js
+++ my-app/src/components/Persons/Person/Person.js
@@ -26,7 +26,7 @@ class Person extends Component {
             <div className="Person" style={this.state.style}>
                 <p onClick={this.props.click}>Hi! My name is {this.props.name}. I'm a freak! and I'm {this.props.age} years old.</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed}/>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
             </div>
         )
     }
--- my-app/src/components/Persons/Persons.js
+++ my-app/src/components/Persons/Persons.js
@@ -1,17 +1,8 @@
  import React, {Component} from 'react'
  import Person from './Person/Person.js';
- import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+ //import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
  
  class Persons extends Component {
-  //  constructor(props) {
-  //    super(props);
-  //  }
-
-  // static getDerivedStateFromProps(props, state) {
-  //   console.log('[Persons.js] getDerivedStateFromProps');
-  //   return state;
-  // }
-
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[Persons.js] shouldComponentUpdate');
     return true;
@@ -30,15 +21,13 @@
     console.log('[Persons.js] rendring...');
     return this.props.persons.map( (person, index) => {
       return (
-        <ErrorBoundary>
         <Person 
+          key={index}
           name={person.name} 
           age={person.age}
           click={ () => this.props.clicked(index)}
           changed={ (event) => {  this.props.changed(event, person.id)} }
-          key={person.id}
         />
-        </ErrorBoundary>
       );
     });
   } 
