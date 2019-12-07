01f61af0cb75b7fd08a820cd5ea4ae31c87136b5
Bohdan Melvic <bohdanmelvic@gmail.com>
2019-12-07 16:34:11 +0200

    added validation form

--- query-app/src/containers/Auth/Auth.js
+++ query-app/src/containers/Auth/Auth.js
@@ -8,6 +8,7 @@ export class Auth extends Component {
         super(props)
     
         this.state = {
+            isFormValid: false,
              formControls: {
                  email: {
                      value: '',
@@ -49,9 +50,50 @@ export class Auth extends Component {
     submitHandler = (event) => {
         event.preventDefault();
     }
+    
+
+    validateControl = (value, validation) => {
+        if (!validation) {
+            return true
+        }
+
+        let isValid = true;
+
+        if (validation.required) {
+            isValid = value.trim() !== ''  && isValid
+        }
+
+        if (validation.email) {
+            isValid = validateEmail(value) && isValid;
+        }
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength  && isValid
+        }
+        return isValid
+    }
 
     onChangeHandler = (event, controlName) => {
         console.log(`${controlName}`, event.target.value);
+
+        const formControls = {...this.state.formControls};
+        const control = {...formControls[controlName]};
+
+        control.value = event.target.value;
+        control.touched = true;
+        control.valid = this.validateControl(control.value, control.validation);
+
+        formControls[controlName] = control;
+
+        let isFormValid = true;
+
+        Object.keys(formControls).forEach( (name) => {
+            isFormValid = formControls[controlName].valid && isFormValid
+        })
+
+        this.setState({
+            formControls,
+            isFormValid
+        })
     }
 
     renderInput = () => {
@@ -85,13 +127,18 @@ export class Auth extends Component {
                 >
                     { this.renderInput() }
 
-                    <Button type='success' onClick={this.loginHandler}>Log In</Button>
-                    <Button type='primary' onClick={this.signUpHandler}>Sign Up</Button>
+                    <Button type='success' onClick={this.loginHandler} disabled={!this.state.isFormValid}>Log In</Button>
+                    <Button type='primary' onClick={this.signUpHandler} disabled={!this.state.isFormValid}>Sign Up</Button>
                 </form>
             </div>
             </div>
         )
     }
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
 export default Auth
