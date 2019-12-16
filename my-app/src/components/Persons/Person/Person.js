bf96884ecf9391edbfb44e0d584116b88da81bff
Bohdan Melvic <bohdanmelvic@gmail.com>
2019-12-16 22:55:56 +0200

    added refs

--- my-app/src/components/Persons/Person/Person.js
+++ my-app/src/components/Persons/Person/Person.js
@@ -14,7 +14,9 @@ class Person extends Component {
                     backgroundColor: '#ccc'
                 }
             }
-        }
+        };
+
+        this.inputElemRef = React.createRef();
     }
 
     //const rnd = Math.random();
@@ -23,6 +25,10 @@ class Person extends Component {
     // throw new Error('some bullshit');
     // }
 
+    componentDidMount() {
+        this.inputElemRef.current.focus();
+    }
+
     render() {
         console.log('[Persons.js] rendring...');
         return (
@@ -30,7 +36,11 @@ class Person extends Component {
             {/* <div className="Person" style={this.state.style}> */}
                 <p onClick={this.props.click}>Hi! My name is {this.props.name}. I'm a freak! and I'm {this.props.age} years old.</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}
+                    ref={this.inputElemRef}
+                />
             {/* </div> */}
             </Auxiliary>
         );
