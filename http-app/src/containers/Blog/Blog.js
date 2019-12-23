9365ed13312f924aad0ec3c5f69cdf724cfe2d74
Bohdan Melvic <bohdanmelvic@gmail.com>
2019-12-24 00:13:00 +0200

    added 2 diff http requests

--- http-app/src/containers/Blog/Blog.js
+++ http-app/src/containers/Blog/Blog.js
@@ -3,18 +3,52 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             posts: [],
+             selectedPostId: null
+        }
+    }
+    
+
+    componentDidMount() {
+        const posts = axios.get('https://jsonplaceholder.typicode.com/posts');
+        posts.then( response => {
+            const posts = response.data.slice(0, 4);
+            const updatedPost = posts.map( p => {
+                return { ...p, author: 'Bohdan' }
+            });
+            this.setState({posts: updatedPost});
+        });
+    }
+
+    postClickedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
     render () {
+        const posts = this.state.posts.map( post => {
+            return (
+                <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postClickedHandler(post.id)}
+                />
+            )
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
