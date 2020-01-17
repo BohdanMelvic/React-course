03f0745bd54e651611c4b3f9105dad0fbc05da4a
Bohdan Melvic <bohdanmelvic@gmail.com>
2020-01-17 18:52:05 +0200

    webpack section

--- webpack-app/src/components/PizzaImage/PizzaImage.js
+++ webpack-app/src/components/PizzaImage/PizzaImage.js
@@ -1,11 +1,9 @@
 import React from 'react';
-
-import classes from './PizzaImage.css';
 import PizzaImage from '../../assets/pizza.jpg';
 
 const pizzaImage = (props) => (
-    <div className={classes.PizzaImage}>
-        <img src={PizzaImage} className={classes.PizzaImg} />
+    <div className='PizzaImage'>
+        <img src={PizzaImage} className='PizzaImg' />
     </div>
 );
 
