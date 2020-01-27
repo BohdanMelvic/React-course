cdb6461ce3e3bcab3974593f9c798af6d5317226
Bohdan Melvic <bohdanmelvic@gmail.com>
2020-01-27 16:13:55 +0200

    removed redux and push context

--- replace-redux/src/index.js
+++ replace-redux/src/index.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import { combineReducers, createStore } from 'redux';
+import ProductsProvider from './context/product-context';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
+//mport productReducer from './store/reducers/products';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
+// const rootReducer = combineReducers({
+//   shop: productReducer
+// });
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+// const app = <Provider store={store}>
+//               <BrowserRouter>
+//                 <App />
+//               </BrowserRouter>
+//             </Provider>;
+
+const app = <ProductsProvider>
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </ProductsProvider>;
+
+ReactDOM.render(app, document.getElementById('root'));
